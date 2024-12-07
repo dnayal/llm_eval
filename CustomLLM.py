42cefd7f205450c9f7af377a297f666d47b382b4
Deepak Nayal <dnayal@gmail.com>
2024-12-07 23:50:05 +0000

    Added support for locally run LLama 1B (or Qwen 1.5B-instruct) model. Works fine now!

--- CustomLLM.py
+++ CustomLLM.py
@@ -1,13 +1,11 @@
-# evaluate NVIDIA & Llama 1B models
-# use custom evaluation
-# use deepeval for evaluation
-# use benchmark scores
+import json
 import os
 from deepeval.models import DeepEvalBaseLLM
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-from langchain_huggingface.llms import HuggingFacePipeline
-from langchain_core.prompts import PromptTemplate
+from pydantic import BaseModel
+from lmformatenforcer import JsonSchemaParser
+from lmformatenforcer.integrations.transformers import build_transformers_prefix_allowed_tokens_fn
 
 # custom model for Nvidia LLM
 class CustomNvidiaLLM(DeepEvalBaseLLM):
@@ -39,44 +37,63 @@ class CustomNvidiaLLM(DeepEvalBaseLLM):
 class CustomLlamaLLM(DeepEvalBaseLLM):
 
     model_name = "meta-llama/Llama-3.2-1B"
-    #model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+    # model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 
     def __init__(self):
-        model = AutoModelForCausalLM.from_pretrained(CustomLlamaLLM.model_name)
-        tokenizer = AutoTokenizer.from_pretrained(CustomLlamaLLM.model_name)
-        pipe = pipeline(
-            task = "text-generation",
-            model = model,
-            tokenizer=tokenizer,
-            temperature=0.1,
-            max_new_tokens=500,
-#            num_return_sequences=1,
-#            eos_token_id=tokenizer.eos_token_id,
-#            pad_token_id=tokenizer.eos_token_id,
-            device_map="auto"
+        model = AutoModelForCausalLM.from_pretrained(
+            CustomLlamaLLM.model_name,
+            device_map = "auto"
         )
-        llm = HuggingFacePipeline(pipeline=pipe)
+        tokenizer = AutoTokenizer.from_pretrained(CustomLlamaLLM.model_name)
+        self.model = model
+        self.tokenizer = tokenizer
 
-        template = """
-        Question: {question} 
-        Instruction: Give specific and precise answers. Do not include anything in your response that does not relate to the question asked.
-        Answer: 
-        """
-        prompt = PromptTemplate.from_template(template)
-        self.chain =  prompt | llm | (lambda output: output.split("Answer:")[-1].strip())
-        
 
     def load_model(self):
-        return self.chain
+        return self.model
 
 
-    def generate(self, prompt: str) -> str:
-        response = self.chain.invoke({"question": prompt})
-        return response
+    def generate(self, prompt: str, schema: BaseModel) -> BaseModel:
+        model = self.load_model()
 
-    async def a_generate(self, prompt: str) -> str:
-        response = await self.chain.ainvoke({"question": prompt})
-        return response
+        pipe = pipeline(
+            "text-generation",
+            model = model,
+            tokenizer = self.tokenizer,
+            use_cache = True,
+            device_map = "auto",
+            max_new_tokens = 500,
+            do_sample = True,
+            top_k = 5,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.eos_token_id
+        )
+
+        # Create parser required for JSON confinement using lmformatenforcer
+        # parser = JsonSchemaParser(schema.schema())
+
+        # CHECK: Comment the following line and uncomment the previous one if you get errors with parsing the schema
+        parser = JsonSchemaParser(schema.model_json_schema())
+        prefix_function = build_transformers_prefix_allowed_tokens_fn(
+            pipe.tokenizer, parser
+        )
+
+        # Output and load valid JSON
+        output_dict = pipe(prompt, prefix_allowed_tokens_fn=prefix_function)
+        output = output_dict[0]["generated_text"][len(prompt) :]
         
+        # CHECK: Uncomment following line if you get errors
+        # For some weird reason printing the JSON output does not throw any error
+        # print("==> Raw Output:", output)  # Debug the raw output
+
+        json_result = json.loads(output)
+
+        # Return valid JSON object according to the schema DeepEval supplied
+        return schema(**json_result)
+
+    async def a_generate(self, prompt: str, schema: BaseModel) -> BaseModel:
+        return self.generate(prompt, schema)
+
     def get_model_name(self):
         return CustomLlamaLLM.get_model_name        
--- Evaluate.py
+++ Evaluate.py
@@ -1,33 +1,56 @@
 from CustomLLM import CustomLlamaLLM, CustomNvidiaLLM
-from deepeval.test_case import LLMTestCase, LLMTestCaseParams
+from deepeval.test_case import LLMTestCase
 from deepeval.metrics import AnswerRelevancyMetric
 from deepeval.benchmarks import MMLU, HellaSwag, BigBenchHard
 from deepeval.benchmarks.tasks import MMLUTask, HellaSwagTask, BigBenchHardTask
 
+question = "who is the president of USA?"
+
 def LLMTestCaseWrapper(question, model):
-    response = model.generate(question)
+    # response = model.generate(question)
     llmtestcase = LLMTestCase(
         input = question,
-        actual_output = response,
+        actual_output = "Joe Biden",#response,
         context = ["Just give me the name of the president - do not give long explainations."]
     )
     return llmtestcase
 
     
-"""
+#################
+# Run tests on local Llama 1B model
+#################
 customllama = CustomLlamaLLM()
+
 llama_relevancy_metric = AnswerRelevancyMetric(
     model=customllama
 )
 llama_relevancy_metric.measure(LLMTestCaseWrapper(question, customllama))
 print(f"[Relevancy] Score: {llama_relevancy_metric.score} Reason: {llama_relevancy_metric.reason}")
-"""
 
+# MMLU Metric
+llama_mmlu_metric = MMLU(
+    tasks=[MMLUTask.HIGH_SCHOOL_COMPUTER_SCIENCE, MMLUTask.ASTRONOMY],
+    n_shots = 5
+)
+llama_mmlu_metric.evaluate(model = customllama)
+print(f"[MMLU]: {llama_mmlu_metric.overall_score}")
+
+# HellaSwag Metric
+llama_hellaswag_metric = HellaSwag(
+    tasks = [HellaSwagTask.TRIMMING_BRANCHES_OR_HEDGES, HellaSwagTask.BATON_TWIRLING, HellaSwagTask.MAKING_A_SANDWICH],
+    n_shots = 5
+)
+llama_hellaswag_metric.evaluate(model = customllama)
+print(f"[HellaSwag]: {llama_hellaswag_metric.overall_score}")
+
+
+#################
+# Run tests on NVIDIA hosted Llama 405B model
+#################
 customnvidia = CustomNvidiaLLM()
 scores = {}
 
 # Relevancy Metric
-question = "who is the president of USA?"
 nvidia_relevancy_metric = AnswerRelevancyMetric(
     model = customnvidia,
     threshold = 0.7
@@ -51,7 +74,7 @@ nvidia_hellaswag_metric = HellaSwag(
 nvidia_hellaswag_metric.evaluate(model = customnvidia)
 scores["HellaSwag"] = nvidia_hellaswag_metric.overall_score
 
-# Big Bench Hard
+# Big Bench Hard Metric
 nvidia_bigbench_metric = BigBenchHard(
     tasks = [BigBenchHardTask.BOOLEAN_EXPRESSIONS, BigBenchHardTask.CAUSAL_JUDGEMENT, BigBenchHardTask.REASONING_ABOUT_COLORED_OBJECTS],
     n_shots = 3,
@@ -63,3 +86,4 @@ scores["Big Bench Hard"] = nvidia_bigbench_metric.overall_score
 print("Evaluation Scores for ", CustomNvidiaLLM.get_model_name())
 for metric, score in scores:
     print(f"[{metric}] : {score}")
+
